30399b41b30199ab39e22f68ed446558793e5bd9
Clay Diffrient <clay.diffrient@gmail.com>
2012-12-18 00:42:40 -0700

    Started work on a JavaFX version.

---
+++ Problem-11/java/ProductOfAdjacents.java
@@ -0,0 +1,66 @@
+/**
+ * This is a solution to Project Euler #11.
+ * It is written as a JavaFX program.
+ * @author Clay Diffrient
+ */
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scence.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ * ProductOfAdjacents
+ *
+ * The main class of the program contains the logic for the graphics.
+ */
+public class ProductOfAdjacents
+   extends Application
+{
+   /**
+    * start
+    *
+    * Contains the initialization parameters to start the application.
+    *
+    * @param primaryStage the stage on which to start.
+    */
+   public void start(Stage primaryStage)
+   {
+      Button btn = new Button().setText("Calculate");
+      StackPane root = new StackPane();
+      btn.setOnAction(new EventHandler<ActionEvent>()
+                      {
+                        public void handle(ActionEvent event)
+                        {
+                           performCalculation();
+                        }
+                      }
+      );
+      primaryStage.setTitle("Products of Adjacents");
+      root.getChildren().add(btn);
+      primaryStage.setScene(new Scene(root, 300, 250));
+      primaryStage.show();
+
+   }
+
+   /**
+    * performCalcuation
+    *
+    * Handles the calculation of the products of adjacent numbers.
+    */
+   public void performCalcuation()
+   {
+      System.out.println("Called");
+   }
+
+   /**
+    * Main is normally ignored in a JavaFX application.  Used for fallback.
+    */
+   public static void main(String[] args)
+   {
+      launch(args);
+   }
+}
