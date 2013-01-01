3d847d691a7bc7defb89a79a515e410c1e39ce50
Clay Diffrient <clay.diffrient@gmail.com>
2013-01-01 15:35:54 -0700

    Modularization and continued work

--- Problem-11/java/ProductOfAdjacents.java
+++ Problem-11/java/ProductOfAdjacents.java
@@ -8,9 +8,14 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.geometry.Pos;
 
 /**
  * ProductOfAdjacents
@@ -29,10 +34,48 @@ public class ProductOfAdjacents
     */
    public void start(Stage primaryStage)
    {
-      Button btn = new Button();
-      btn.setText("Calculate");
-      StackPane root = new StackPane();
-      btn.setOnAction(new EventHandler<ActionEvent>()
+
+      BorderPane root = new BorderPane();
+
+      TextArea matrixEntry = new TextArea();
+      primaryStage.setTitle("Products of Adjacents");
+
+      root.setTop(new Text("Sum Adjacents"));
+      root.setBottom(generateButtonPanel());
+      root.setCenter(matrixEntry);
+      primaryStage.setScene(new Scene(root, 300, 250));
+      primaryStage.show();
+
+   }
+
+   /**
+    * generateTitleSection
+    *
+    * Creates the title/instruction section for the UI
+    *
+    * @return HBox
+    */
+   private HBox generateTitleSection()
+   {
+      HBox titleSection = new HBox(10);
+      Text title = new Text("Product of Adjacents");
+      title.setFont(Font.font("Verdana", 40));
+      titleSection.getChildren().addAll(title);
+      return titleSection;
+   }
+
+   /**
+    * generateButtonPanel
+    *
+    * Creates the button panel needed for the the UI.
+    *
+    * @return HBox
+    */
+   private HBox generateButtonPanel()
+   {
+      Button calcBtn = new Button();
+      calcBtn.setText("Calculate");
+      calcBtn.setOnAction(new EventHandler<ActionEvent>()
                       {
                         public void handle(ActionEvent event)
                         {
@@ -40,11 +83,18 @@ public class ProductOfAdjacents
                         }
                       }
       );
-      primaryStage.setTitle("Products of Adjacents");
-      root.getChildren().add(btn);
-      primaryStage.setScene(new Scene(root, 300, 250));
-      primaryStage.show();
-
+      Button exitBtn = new Button("Exit");
+      exitBtn.setOnAction(new EventHandler<ActionEvent>()
+                     {
+                        public void handle(ActionEvent event)
+                        {
+                           System.exit(1);
+                        }
+                     });
+      HBox bottomPanel = new HBox(10);
+      bottomPanel.getChildren().addAll(calcBtn, exitBtn);
+      bottomPanel.setAlignment(Pos.CENTER);
+      return bottomPanel;
    }
 
    /**
