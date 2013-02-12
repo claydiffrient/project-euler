dcb3aeed8fb20eda3de8984fcf8b6ce5e8cab6ba
Clay Diffrient <clay.diffrient@gmail.com>
2013-02-11 21:50:37 -0700

    Completed problem 18, needs documenting. Earned Level 1 status

--- Problem-18/java/MaxPathSum.java
+++ Problem-18/java/MaxPathSum.java
@@ -4,32 +4,125 @@
 
 import java.util.List;
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+
 
 public class MaxPathSum
    implements Runnable
 {
    String mFileName;
-   List<List<Integer>> mTree;
+   BufferedReader mFileReader;
+   int[][] mTriangle;
+   int mDepth;
 
    public MaxPathSum(String pFileName)
    {
       mFileName = pFileName;
-      mTree = new ArrayList<ArrayList<Integer>>();
+      try
+      {
+         mFileReader = new BufferedReader(new FileReader(mFileName));
+      }
+      catch (Exception e)
+      {
+         System.out.println("Error");
+      }
+      mDepth = getDepth();
+      mTriangle = new int[mDepth][];
+   }
+
+   private int getDepth()
+   {
+      int lines = 0;
+      try
+      {
+         while (mFileReader.readLine() != null)
+         {
+            lines++;
+         }
+         mFileReader.close();
+      }
+      catch(Exception e)
+      {
+         System.out.println("Error Reading.");
+      }
+      System.out.println(lines);
+      return lines;
+   }
+
+   private void fillTriangle()
+   {
+      try
+      {
+         mFileReader = new BufferedReader(new FileReader(mFileName));
+      }
+      catch(Exception e)
+      {
+         System.out.println("Error.");
+      }
+      String string;
+      int i = 0;
+      try
+      {
+         while(( string = mFileReader.readLine()) != null)
+         {
+            mTriangle[i] = new int[i + 1];
+            int j = 0;
+            Scanner numbers = new Scanner(string);
+            while (numbers.hasNext())
+            {
+               int value = numbers.nextInt();
+               mTriangle[i][j] = value;
+               j++;
+            }
+            i++;
+         }
+      }
+      catch(Exception e)
+      {
+         System.out.println("IO Error");
+         e.printStackTrace();
+      }
+   }
+
+   private int[] getRowMax(int[] pPrev, int[] pLast)
+   {
+      for (int i = 0; i < pPrev.length; i++)
+      {
+         pPrev[i] = pPrev[i] + (Math.max(pLast[i], pLast[i+1]));
+      }
+      return pPrev;
+   }
+
+   private int getMax()
+   {
+      int[] previous = null;
+      for (int i = 1; i < mDepth; i++)
+      {
+         int[] last = mTriangle[mDepth - i];
+         previous = getRowMax(mTriangle[(mDepth -i) -1], last);
+      }
+      return previous[0];
    }
 
    public void run()
    {
-
-
+      fillTriangle();
+      System.out.println("Max sum is:" + getMax());
    }
 
    public static void main(String[] args)
    {
-      if (args[0] == null)
+      try
       {
-         System.out.println("Invalid input.");
+         new MaxPathSum(args[0]).run();
+      }
+      catch(Exception e)
+      {
+         System.out.println("Invalid input." + e.getMessage());
          System.exit(1);
       }
-      new MaxPathSum(args[0]).run();
+
    }
 }
