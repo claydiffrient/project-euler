37a528942d6319f58267c85cf13550fbec51ed8a
Clay Diffrient <clay.diffrient@gmail.com>
2013-02-12 12:33:04 -0700

    First try at 56

---
+++ Problem-56/java/DigitSum.java
@@ -0,0 +1,58 @@
+/**
+ * Solution to Euler #56
+ */
+
+import java.math.BigInteger;
+
+public class DigitSum
+   implements Runnable
+{
+   int mLimit;
+   int mMaxSum;
+
+   public DigitSum(int pLimit)
+   {
+      mLimit = pLimit;
+      mMaxSum = 0;
+   }
+
+   public int getDigitSum(BigInteger pInt)
+   {
+      int sum = 0;
+      String string = pInt.toString();
+      for (int i = 0; i < string.length(); i++)
+      {
+         sum += Character.getNumericValue(string.charAt(i));
+      }
+      return sum;
+   }
+
+   public void run()
+   {
+      for (int a = 0; a < mLimit; a++)
+      {
+         BigInteger aBig = new BigInteger(String.valueOf(a));
+         for (int b = 0; a < mLimit; b++)
+         {
+            int value = getDigitSum(aBig.pow(b));
+            if (value > mMaxSum)
+            {
+               mMaxSum = value;
+            }
+         }
+      }
+      System.out.println("Max digital sum is: " + mMaxSum);
+   }
+
+   public static void main(String[] args)
+   {
+      try
+      {
+         new DigitSum(Integer.parseInt(args[0])).run();
+      }
+      catch (Exception e)
+      {
+         System.out.println("Error");
+      }
+   }
+}
