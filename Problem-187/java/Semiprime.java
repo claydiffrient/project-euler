e8b597debf320e473fca6776bd4112a98389d5dc
Clay Diffrient <clay.diffrient@gmail.com>
2013-02-18 20:36:19 -0700

    First attempt at 187, brute force

---
+++ Problem-187/java/Semiprime.java
@@ -0,0 +1,54 @@
+/**
+ * Solution to Project Euler #187
+ */
+
+public class Semiprime
+   implements Runnable
+{
+   int mLessThan;
+
+   public Semiprime(int pLessThan)
+   {
+      mLessThan = pLessThan;
+   }
+
+   private int getNumPrimeFactors(int pNum)
+   {
+      int count = 0;
+      for (int i = 2; i <= pNum; i++)
+      {
+         while (pNum % i == 0)
+         {
+            count++;
+            pNum /= i;
+         }
+      }
+      return count;
+   }
+
+   public void run()
+   {
+      int num = 0;
+      for (int i = 0; i < mLessThan; i++)
+      {
+         System.out.println("Checking:" + i);
+         if (getNumPrimeFactors(i) == 2)
+         {
+            num++;
+         }
+      }
+      System.out.println(num);
+   }
+
+   public static void main(String[] args)
+   {
+      try
+      {
+         new Semiprime(Integer.parseInt(args[0])).run();
+      }
+      catch(Exception e)
+      {
+         System.out.println("Error");
+      }
+   }
+}
