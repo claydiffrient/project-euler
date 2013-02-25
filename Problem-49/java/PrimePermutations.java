258c0b89dc17586626d0de2f27905992938f5e37
Clay Diffrient <clay.diffrient@gmail.com>
2013-02-25 13:06:01 -0700

    First attempt, no luck so far

---
+++ Problem-49/java/PrimePermutations.java
@@ -0,0 +1,124 @@
+/**
+* Solution to Project Euler #49.
+*
+* 4 digit prime permutations which are all prime and
+*
+* Problem Link: http://projecteuler.net/problem=49
+* Github Issue: https://github.com/claydiffrient/project-euler/issues/Project Number + 7
+*
+* Created by: Clay Diffrient
+* Email: clay.diffrient@gmail.com
+* Github/Twitter: @claydiffrient
+* Project Euler Account: http://projecteuler.net/profile/claydiffrient.png
+*/
+
+import java.util.Arrays;
+
+public class PrimePermutations
+   implements Runnable
+{
+   int mPrimeDigits;
+   boolean[] mPrimes;
+
+   /**
+    * Default constructor
+    */
+   public PrimePermutations()
+   {
+      mPrimeDigits = 10000;
+      mPrimes = new boolean[10000];
+      Arrays.fill(mPrimes, true);
+   }
+
+   private void sievePrimes()
+   {
+      for (int i = 2; i < (int) Math.sqrt(mPrimeDigits); i++)
+      {
+         if (mPrimes[i])
+         {
+            for (int j = i*i; j < mPrimeDigits; j = j+i)
+            {
+               mPrimes[j] = false;
+            }
+         }
+      }
+   }
+
+   private boolean isPermutation(int pFirst, int pSecond)
+   {
+      String one = String.valueOf(pFirst);
+      String two = String.valueOf(pSecond);
+      if (one.length() != two.length())
+      {
+         return false;
+      }
+      char[] oneA = one.toCharArray();
+      char[] twoA = two.toCharArray();
+      int count = 0;
+      for (int i = 0; i < oneA.length; i++)
+      {
+         for (int j = 0; j < twoA.length; j++)
+         {
+            if (twoA[j] == oneA[i])
+            {
+               count++;
+            }
+         }
+      }
+      if (count == one.length())
+      {
+         return true;
+      }
+      return false;
+   }
+
+   /**
+    * Run all the code in a thread.
+    */
+   public void run()
+   {
+      int thisPrime = 0;
+      int nextPrime = 0;
+      int thirdPrime = 0;
+      int count = 0;
+      sievePrimes();
+      for (int i = 1000; i < 9999; i++)
+      {
+         if (mPrimes[i])
+         {
+            thisPrime = nextPrime;
+            nextPrime = i;
+            if (nextPrime != 0)
+            {
+               if (isPermutation(thisPrime, nextPrime))
+               {
+                  for (int j = 1000; j < 9999; j++)
+                  {
+                     if (mPrimes[j])
+                     {
+                        thirdPrime = j;
+                        if (isPermutation(nextPrime, thirdPrime))
+                        {
+                           System.out.println(thisPrime + ", " + nextPrime +
+                                              ", " + thirdPrime);
+                        }
+                     }
+                  }
+               }
+            }
+         }
+      }
+   }
+
+   /**
+    * Main
+    * @param args command-line input
+    */
+   public static void main(String[] args)
+   {
+      new PrimePermutations().run();
+   }
+
+}
+
+
