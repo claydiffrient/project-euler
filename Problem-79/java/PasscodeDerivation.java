1d9ba72560b61f454d9e11ec3922b42714bbbe3b
Clay Diffrient <clay.diffrient@gmail.com>
2013-02-25 16:07:59 -0700

    Starting 79, nothing noteworthy yet

---
+++ Problem-79/java/PasscodeDerivation.java
@@ -0,0 +1,45 @@
+/**
+* Solution to Project Euler #79.
+*
+* Makes a keycode solution
+*
+* Problem Link: http://projecteuler.net/problem=79
+* Github Issue: https://github.com/claydiffrient/project-euler/issues/86
+*
+* Created by: Clay Diffrient
+* Email: clay.diffrient@gmail.com
+* Github/Twitter: @claydiffrient
+* Project Euler Account: http://projecteuler.net/profile/claydiffrient.png
+*/
+
+public class PasscodeDerivation
+   implements Runnable
+{
+   /**
+    * Default constructor
+    */
+   public PasscodeDerivation()
+   {
+
+   }
+
+   /**
+    * Run all the code in a thread.
+    */
+   public void run()
+   {
+
+   }
+
+   /**
+    * Main
+    * @param args command-line input
+    */
+   public static void main(String[] args)
+   {
+      new PasscodeDerivation().run();
+   }
+
+}
+
+
