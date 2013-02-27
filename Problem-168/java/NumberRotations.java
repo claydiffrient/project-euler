5e654ce91c7bcd471003c1c1b1df41986b26b911
Clay Diffrient <clay.diffrient@gmail.com>
2013-02-27 09:51:25 -0700

    Initial commit of problem 168

---
+++ Problem-168/java/NumberRotations.java
@@ -0,0 +1,47 @@
+/**
+* Solution to Project Euler #168.
+*
+* Finds the sum of all integers that are the sum of their right rotations between
+* 10 and 10^100.
+*
+*
+* Problem Link: http://projecteuler.net/problem=168
+* Github Issue: https://github.com/claydiffrient/project-euler/issues/175
+*
+* Created by: Clay Diffrient
+* Email: clay.diffrient@gmail.com
+* Github/Twitter: @claydiffrient
+* Project Euler Account: http://projecteuler.net/profile/claydiffrient.png
+*/
+
+public class NumberRotations
+   implements Runnable
+{
+   /**
+    * Default constructor
+    */
+   public NumberRotations()
+   {
+
+   }
+
+   /**
+    * Run all the code in a thread.
+    */
+   public void run()
+   {
+
+   }
+
+   /**
+    * Main
+    * @param args command-line input
+    */
+   public static void main(String[] args)
+   {
+      new NumberRotations().run();
+   }
+
+}
+
+
